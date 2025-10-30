11503f8ada599793ceada71c0cd380d46cf49cf2
Curve <fynnbwdt@gmail.com>
2025-10-31 00:58:19 +0100

    refactor(lib): exceptions are evil

--- lib/model.ts
+++ lib/model.ts
@@ -1,9 +1,17 @@
+import { Diagnostic } from "vscode-languageserver";
 import { AstNode, EmptyFileSystem, LangiumDocument, LangiumServices, URI } from "langium";
 
 import type { Model } from "../src/language/generated/ast";
-import { createJavaScriptPropositionalLaboratoryFormatServices as createJSPLFormatServices } from "../src/language/java-script-propositional-laboratory-format-module.js";
+import { createJavaScriptPropositionalLaboratoryFormatServices as createJSPLFormatServices } from "../src/language/java-script-propositional-laboratory-format-module";
 
-export async function extractDocument(content: string, services: LangiumServices): Promise<LangiumDocument>
+import { err, ok, Result } from "neverthrow";
+
+type Res<T> = Result<T, Diagnostic[]>;
+
+export async function extractDocument(
+    content: string,
+    services: LangiumServices,
+): Promise<Res<LangiumDocument<AstNode>>>
 {
     const document = services.shared.workspace.LangiumDocumentFactory.fromString(content, URI.file("tmp.jspl"));
     await services.shared.workspace.DocumentBuilder.build([document], { validation: true });
@@ -11,18 +19,18 @@ export async function extractDocument(content: string, services: LangiumServices
 
     if (validationErrors.length > 0)
     {
-        throw validationErrors;
+        return err(validationErrors);
     }
 
-    return document;
+    return ok(document);
 }
 
-export async function extractAstNode<T extends AstNode>(fileName: string, services: LangiumServices): Promise<T>
+export async function extractAstNode<T extends AstNode>(fileName: string, services: LangiumServices): Promise<Res<T>>
 {
-    return (await extractDocument(fileName, services)).parseResult?.value as T;
+    return (await extractDocument(fileName, services)).andThen(doc => ok(doc.parseResult?.value as T));
 }
 
-export function extractModel(input: string): Promise<Model>
+export function extractModel(input: string): Promise<Res<Model>>
 {
     const services = createJSPLFormatServices(EmptyFileSystem).JavaScriptPropositionalLaboratoryFormat;
     return extractAstNode<Model>(input, services);
