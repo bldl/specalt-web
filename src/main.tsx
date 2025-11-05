41502661593a22c3ed8745d76bdcb8e9320d8a47
Curve <fynnbwdt@gmail.com>
2025-11-05 21:08:56 +0100

    feat(frontend): improve layout

--- src/main.tsx
+++ src/main.tsx
@@ -8,38 +8,41 @@ import { ActionIcon, AppShell, Group, MantineProvider, Text } from "@mantine/cor
 
 import Editor from "./pages/editor.tsx";
 import { initMonaco } from "./utils/monaco.ts";
+import { ModalsProvider } from "@mantine/modals";
 
 const languageConfig = await initMonaco();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <MantineProvider theme={{ primaryColor: "violet", defaultRadius: "md" }} forceColorScheme="dark">
-        <AppShell padding="md" header={{ height: 60 }}>
-            <AppShell.Header>
-                <Group wrap="nowrap" justify="space-between" h="100%" px="md">
-                    <Group wrap="nowrap" gap="xs">
-                        <IconTestPipe />
-                        <Text fw="bolder">
-                            SpecAlt
-                        </Text>
-                        <Text c="dimmed" fs="italic" visibleFrom="md">
-                            - The JavaScript Proposal Laboratory
-                        </Text>
+        <ModalsProvider>
+            <AppShell padding="md" header={{ height: 60 }}>
+                <AppShell.Header>
+                    <Group wrap="nowrap" justify="space-between" h="100%" px="md">
+                        <Group wrap="nowrap" gap="xs">
+                            <IconTestPipe />
+                            <Text fw="bolder">
+                                SpecAlt
+                            </Text>
+                            <Text c="dimmed" fs="italic" visibleFrom="md">
+                                - The JavaScript Proposal Laboratory
+                            </Text>
+                        </Group>
+                        <Group>
+                            <ActionIcon
+                                component="a"
+                                href="https://github.com/bldl/specalt-web"
+                                target="_blank"
+                                variant="light"
+                            >
+                                <IconBrandGithub size={16} />
+                            </ActionIcon>
+                        </Group>
                     </Group>
-                    <Group>
-                        <ActionIcon
-                            component="a"
-                            href="https://github.com/bldl/specalt-web"
-                            target="_blank"
-                            variant="light"
-                        >
-                            <IconBrandGithub size={16} />
-                        </ActionIcon>
-                    </Group>
-                </Group>
-            </AppShell.Header>
-            <AppShell.Main>
-                <Editor languageConfig={languageConfig} />
-            </AppShell.Main>
-        </AppShell>
+                </AppShell.Header>
+                <AppShell.Main>
+                    <Editor languageConfig={languageConfig} />
+                </AppShell.Main>
+            </AppShell>
+        </ModalsProvider>
     </MantineProvider>,
 );
