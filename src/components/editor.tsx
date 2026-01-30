f150facc86c92e7953a1419d7a7bd6d6f7015bc6
Curve <fynnbwdt@gmail.com>
2026-01-30 20:12:40 +0100

    refactor(editor): load asynchronously

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,8 +1,11 @@
 import * as vscode from "vscode";
 import * as monaco from "@codingame/monaco-vscode-editor-api";
 
+import { useEffect, useState } from "react";
 import { initMonaco, vscodeConfig } from "../utils/monaco";
+import { LanguageClientConfig } from "monaco-languageclient/lcwrapper";
 import { MonacoEditorProps, MonacoEditorReactComp } from "@typefox/monaco-editor-react";
+import { Loader, Stack } from "@mantine/core";
 
 type Omitted = "vscodeApiConfig" | "languageClientConfig" | "onVscodeApiInitDone" | "onEditorStartDone";
 
@@ -12,10 +15,26 @@ export interface EditorProps extends Omit<MonacoEditorProps, Omitted>
     updateLab: (value: string) => void;
 }
 
-const languageConfig = await initMonaco();
+const languageConfigPromise = initMonaco();
 
 export function Editor({ updateLab, lastDraft, ...props }: EditorProps)
 {
+    const [languageConfig, setLanguageConfig] = useState<LanguageClientConfig | undefined>();
+
+    useEffect(() =>
+    {
+        languageConfigPromise.then(setLanguageConfig);
+    }, []);
+
+    if (!languageConfig)
+    {
+        return (
+            <Stack className="editor" align="center" justify="center">
+                <Loader />
+            </Stack>
+        );
+    }
+
     return (
         <MonacoEditorReactComp
             {...props}
